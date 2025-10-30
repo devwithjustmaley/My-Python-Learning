353286d8f2b9340b08b61b445cec9f82655b0078
JustMaley <macbookair@JustMaley.local>
2025-10-30 20:42:32 +0000

    Ask the user if want to continue the tictactoe game

--- Projets/tictactoe.py
+++ Projets/tictactoe.py
@@ -4,16 +4,24 @@ current_player = "X"
 emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]
 
 def show_dashboard():
+    print("-" * 13)
     for i in range(9):
-        print(dashboard[i] if dashboard[i] != empty else emojis[i], end=" ")
+        print("|", dashboard[i] if dashboard[i] != empty else emojis[i], end=" ")
         if i % 3 == 2:
-            print("")
+            print("|")
+            print("-" * 13)
+    print("")
+
+print("")
+print("Bienvenue dans le jeu TicTacToe !")
+print("")
 
 while True:
     player_choice = 0
     show_dashboard()
     try:
-        player_choice = int(input(f"Joueur {current_player} "))
+        print("Choisissez la case voulue")
+        player_choice = int(input(f"Joueur {current_player} : "))
         if player_choice > 9 or player_choice < 1:
             print("Vous devez choisir une valeur entre 1 et 9 ")
             continue
@@ -32,8 +40,16 @@ while True:
             or empty != dashboard[2] == dashboard[4] == dashboard[6] \
             or empty != dashboard[0] == dashboard[4] == dashboard[8]:
                 show_dashboard()
-                print(f"Le joueur {current_player} gagne la partie")
-                break
+                print(f" 🎉🎉🎉 Le joueur {current_player} gagne la partie")
+                
+                choice = input("Voulez vous une autre partie ? Oui(O) ou Non(N) ")
+                if choice == "O":
+                    dashboard = [empty for i in range(9)]
+                    continue
+                elif choice == "N":
+                    break
+                else:
+                    print("Entrez Oui ou Non")
 
             current_player = "O" if current_player == "X" else "X"
             continue
